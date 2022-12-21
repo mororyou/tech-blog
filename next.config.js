83d1fad7310d0f2309ed047fcbd91e8433649622
tomare-moroizumi <m.ryousuke0401@icloud.com>
2022-12-21 23:37:27 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['127.0.0.1', 'storage.googleapis.com'],
+  },
+  experimental: {
+    appDir: true,
+  },
 }
 
 module.exports = nextConfig
